e1cd3157821df5cdb8c828d5d7c82364b84d9e01
ljn425 <ljn425@naver.com>
2023-09-04 13:08:41 +0900

    OAuth2 자동설정에 의한 초기화

--- src/main/java/springsecurity/oauth2client/IndexController.java
+++ src/main/java/springsecurity/oauth2client/IndexController.java
@@ -1,12 +1,28 @@
 package springsecurity.oauth2client;
 
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public IndexController(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
     @GetMapping("/")
     public String index() {
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+
+        String clientId = clientRegistration.getClientId();
+        System.out.println("clientId = " + clientId);
+
+        String redirectUri = clientRegistration.getRedirectUri();
+        System.out.println("redirectUri = " + redirectUri);
         return "Hello World!";
     }
 }
