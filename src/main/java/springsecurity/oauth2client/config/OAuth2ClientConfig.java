aa7deaeef6848bd5eb7489faac0704bf949a0c08
ljn425 <ljn425@naver.com>
2023-09-11 15:07:07 +0900

    OAuth2 DefaultOAuth2AuthorizedClientManager 필터 기반으로 구현하기

--- src/main/java/springsecurity/oauth2client/config/OAuth2ClientConfig.java
+++ src/main/java/springsecurity/oauth2client/config/OAuth2ClientConfig.java
@@ -6,12 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import springsecurity.oauth2client.filter.CustomOAuth2AuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class OAuth2ClientConfig {
+
+    private final DefaultOAuth2AuthorizedClientManager authorizedClientManager; // 권한부여 클라이언트 매니저
+    private final OAuth2AuthorizedClientRepository authorizedClientRepository; // 권한부여 클라이언트 저장소
+
     @Bean
     SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(requests -> requests
@@ -23,6 +33,18 @@ public class OAuth2ClientConfig {
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID")
                 .clearAuthentication(true));
+        http.addFilterBefore(customOAuth2AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        
         return http.build();
+    }
+
+    private CustomOAuth2AuthenticationFilter customOAuth2AuthenticationFilter() {
+        CustomOAuth2AuthenticationFilter customOAuth2AuthenticationFilter =
+                new CustomOAuth2AuthenticationFilter(authorizedClientManager, authorizedClientRepository);
+
+        customOAuth2AuthenticationFilter.setAuthenticationSuccessHandler((request, response, authentication) -> {
+            response.sendRedirect("/home");
+        });
+        return customOAuth2AuthenticationFilter;
     }
 }
--- src/main/java/springsecurity/oauth2client/controller/HomeController.java
+++ src/main/java/springsecurity/oauth2client/controller/HomeController.java
@@ -1,13 +1,27 @@
 package springsecurity.oauth2client.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final OAuth2AuthorizedClientService authorizedClientService;
 
     @GetMapping("/home")
-    public String home() {
+    public String home(OAuth2AuthenticationToken oAuth2AuthenticationToken, Model model) {
+        OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient("keycloak", oAuth2AuthenticationToken.getName());
+        model.addAttribute("AccessToken", authorizedClient.getAccessToken().getTokenValue());
+        model.addAttribute("RefreshToken", authorizedClient.getRefreshToken().getTokenValue());
+
+
+
         return "home";
     }
 }
